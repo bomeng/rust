b35307f0e4940ed72782fe2b1881f0277f73ef14
Bo Meng <mengbo@hotmail.com>
2023-05-04 21:33:40 -0700

    add files

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ fn main() {
     let x = 4;
     println!("x is: {}", x);
     {
-       let x = 2;
-       println!("x is: {}", x);
+        let x = 2;
+        println!("x is: {}", x);
     }
     let x = x + 1;
     println!("x is: {}", x);
