7efbd321c8dbc43c791412d1fed29bd0e95c02e7
Bo Meng <mengbo@hotmail.com>
2023-05-04 20:47:46 -0700

    add files

--- hello-world/target/debug/deps/hello_world-8c6eda8e9672aa3e.d
+++
@@ -1,5 +0,0 @@
-/Users/mengbo/github/rust/hello-world/target/debug/deps/hello_world-8c6eda8e9672aa3e: src/main.rs
-
-/Users/mengbo/github/rust/hello-world/target/debug/deps/hello_world-8c6eda8e9672aa3e.d: src/main.rs
-
-src/main.rs:
--- hello-world/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-/Users/mengbo/github/rust/hello-world/target/debug/hello-world: /Users/mengbo/github/rust/hello-world/src/main.rs
