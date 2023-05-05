b4e0d65f031e245ed3d70768f482c106e9bab149
Bo Meng <mengbo@hotmail.com>
2023-05-04 21:21:55 -0700

    add files

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
+    const SECOND_IN_MINUTE: u32 = 60;
+    println!("{}", SECOND_IN_MINUTE);
+    let x = 4;
+    println!("x is: {}", x);
+    {
+       let x = 2;
+       println!("x is: {}", x);
+    }
+    let x = x + 1;
+    println!("x is: {}", x);
     println!("Hello, world!");
 }
