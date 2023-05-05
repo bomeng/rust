3faa1d506df6a1f8b36c4014ceabc85ba70f1cc7
Bo Meng <mengbo@hotmail.com>
2023-05-04 22:11:31 -0700

    add files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#![allow(unused)]
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+
 fn main() {
     const SECOND_IN_MINUTE: u32 = 60;
     println!("{}", SECOND_IN_MINUTE);
@@ -9,5 +17,19 @@ fn main() {
     }
     let x = x + 1;
     println!("x is: {}", x);
-    println!("Hello, world!");
+
+    println!("What is your name?");
+    let mut name: String = String::new();
+    let greeting: &str = "Nice to meet you";
+    io::stdin().read_line(&mut name)
+        .expect("Didn't Receive Input");
+    println!("Hello, {}! {}", name.trim_end(), greeting);
+
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.141592;
+    let age: &str = "47";
+    let mut age: u32 = age.trim().parse()
+        .expect("Age wasn't assigned a number");
+    age = age + 1;
+    println!("I'm {} and I want ${}", age, ONE_MIL);
 }
